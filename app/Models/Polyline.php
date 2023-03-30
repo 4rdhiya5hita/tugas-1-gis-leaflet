7970fe5805d5205872cbe1b23eed1c4eeee03116
Ni Made Ardhiya Shita Pramesti Dewi <117797716+4rdhiya5hita@users.noreply.github.com>
2023-03-31 05:45:44 +0800

    Add files via upload

---
+++ app/Models/Polyline.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Polyline extends Model
+{
+    use HasFactory;
+    
+    protected $table = "tb_polyline";
+    protected $fillable = ['latitude', 'longitude'];
+}
